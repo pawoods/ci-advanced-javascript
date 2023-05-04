f2b25e6a4565f1992a76bab423569bc03fee7ede
Paul Woods <82415999+pawoods@users.noreply.github.com>
2023-05-04 11:26:57 +0100

    Update and rename arrow-functions.js to advanced-javascript.js

---
+++ advanced-javascript.js
@@ -0,0 +1,27 @@
+/**
+ * To run this file in Gitpod, use the 
+ * command node advanced-javascript.js in the terminal
+ */
+
+//----------ARROW FUNCTIONS----------
+
+// Vanilla JavaScript Function
+
+// Arrow Function With Parameters
+
+// Single Line Arrow Function With Parameters
+
+// Implicit Returns
+
+// Returning Multiple Lines
+
+//----------SPREAD OPERATOR----------
+
+
+
+//----------REST PARAMETER----------
+
+
+
+//----------DESTRUCTURING----------
+
